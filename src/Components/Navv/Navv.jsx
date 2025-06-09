879378124bd56a2c73ce5c466b7e1e1d3440f97e
Solaja Emmanuel <emmanueladebola98@gmail.com>
2025-06-09 18:12:32 +0100

    Typo corrected

--- src/Components/Navv/Navv.jsx
+++ src/Components/Navv/Navv.jsx
@@ -1,4 +1,4 @@
-import Styles from './navv.module.css'
+import Styles from './Navv.module.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
